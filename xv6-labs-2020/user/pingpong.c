4d75b76c26f1fe81e7a1aa39ca9024d30cceda3b
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-04-05 18:00:49 +0100

    pingpong

---
+++ xv6-labs-2020/user/pingpong.c
@@ -0,0 +1,35 @@
+// pingpong.c
+// author : Junnan Liu
+// date: 05/04/2024
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int p[2];
+  char *str;
+  if(pipe(p)<0){
+    fprintf(2,"error for pipe\n");
+    exit(1);
+  }
+  int pro_num = getpid();
+  if(fork() == 0){
+    read(p[0],str,4);
+    close(p[0]);
+    write(p[1],"pong",4);
+    close(p[1]);
+    fprintf(1,"%d : received %s\n",pro_num,str);
+  }
+  else{
+    read(p[0],str,4);
+    close(p[0]);
+    write(p[1],"ping",4);
+    close(p[1]);
+    fprintf(1,"%d : received %s\n",pro_num,str);
+    wait(0);
+  }
+  exit(0);
+}
