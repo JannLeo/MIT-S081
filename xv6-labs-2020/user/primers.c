f8d45dd14933b74e54030fe37c613732991aa283
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-04-11 21:39:30 +0100

    Update primers.c

--- xv6-labs-2020/user/primers.c
+++ xv6-labs-2020/user/primers.c
@@ -1,58 +1,70 @@
 // primers.c
 // Author : Junnan Liu
 // date: 05/04/2024
+// update : 11/04/2024
 
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
 
+void DFS(int num,int **p,int pipe_num){
+    int flag = 0;
+    int i;
+    pipe_num %= 34;
+    if (pipe(p[pipe_num]) < 0) {
+        fprintf(2, "Error creating pipe\n");
+        exit(2);
+    }
+    int pid = fork();
+    while(read(p[pipe_num][0],&i,sizeof(int))){
+        if(i%num != 0 ){
+            if(!flag && pid == 0){
+                fprintf(1,"%d\n",i);
+                DFS(i,p,pipe_num+1);
+                flag = 1;
+            }
+            else if (pid < 0){
+                fprintf(2, "Error forking\n");
+                exit(2);
+            }
+            else{
+                write(p[pipe_num+1][1],&i,sizeof(int));
+            }
+        }
+    }
+    wait(0);
+    close(p[pipe_num][0]);
+    close(p[pipe_num][1]);
+    exit(0);
+    
+}
+
 int
 main(int argc, char *argv[])
 {
+    //二维矩阵表示每个进程的管道
     int p[34][2];
     int num=2;
-    int flag=0;
+    int flag=1;
     fprintf(1,"%d\n",num);
-    //主进程
-    for(int i=num+1;i<=35;i++){
-        if(i==flag && pipe(p[i-3])<0){
-            fprintf(2,"error for pipe\n");
-            exit(1);
-        }
-        //子进程 但是只能等到主进程产生之后才能read
-        if(i==flag && fork()==0){
-            int n;
-            int flagg=0;
-            while(read(p[i-3][0],&n,sizeof(int))){
-                if(!flagg){
-                    num=n;
-                    fprintf(1,"%d\n",n);
-                    flagg=num;
-                    flag = num;
-                }
-                if(num<n && n%num!=0){
-                    write(p[num-3][1],&n,sizeof(int));
-                }
-                
-            }
-            if(!flagg){
-                close(p[i-3][1]);
-            }
-            close(p[i-3][0]);
-        }
-        //主进程
-        else{
-            if(i%num!=0){
-                write(p[i-3][1],&i,sizeof(int));
-                if(!flag){
-                    flag=i;
-                }
-            }
-        }
-        
+    if (pipe(p[0]) < 0) {
+        fprintf(2, "Error creating pipe\n");
+        exit(2);
     }
+    int pid = fork();
+    for(int i=num+1;i<=35;i++){
+        if(i%num!= 0 ){
+            if(pid==0 && flag){
+                DFS(num,p,0);
+                flag = 0;
+            }
+            else if(pid != 0){
+                write(p[1][1],&i,sizeof(int));
+            }
+        }
+    }
+    wait(0);
     close(p[0][0]);
     close(p[0][1]);
-    
     exit(0);
 }
