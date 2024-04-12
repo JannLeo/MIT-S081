423efbb258bacd220a4765250df939a4c8cabedc
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-04-12 21:06:21 +0100

    Update primers.c

--- xv6-labs-2020/user/primers.c
+++ xv6-labs-2020/user/primers.c
@@ -13,7 +13,8 @@ void DFS(int fd){
     int i;
     int num;
     //读取第一个值
-    read (fd,&num,sizeof(int));
+    if(!read (fd,&num,sizeof(int)))
+        exit(0);
     printf("Thead(%d) prime %d\n", getpid(), num);
     fprintf(1, "prime %d \n",num);
     //创建管道
@@ -22,30 +23,30 @@ void DFS(int fd){
         fprintf(2, "Error creating pipe\n");
         exit(1);
     }
-    int temp = -1;
-    while(1){
-        temp = read(fd,&i,sizeof(int));
-        if(temp < 0)
-            break;
-        if(i % num != 0 ){
-            // fprintf(1, "Write successfully %d DFS\n",i-2);
-            write(p[1],&i,sizeof(int));
-        }
-    }
-    if(i == -1){
-        fprintf(1, "FINISH\n",i-2);
-        close(p[1]);
-        close(p[0]);
-        close(fd);
-        return;
-    }
-    int pid = fork();
-    if(pid == 0){
+    if(fork() == 0){
         close(p[1]);
         close(fd);
         DFS(p[0]);
         close(p[0]);
-    }else{
+    }
+    else{
+        int temp = -1;
+        while(1){
+            temp = read(fd,&i,sizeof(int));
+            if(temp < 0)
+                break;
+            if(i % num != 0 ){
+                // fprintf(1, "Write successfully %d DFS\n",i-2);
+                write(p[1],&i,sizeof(int));
+            }
+        }
+        if(i == -1){
+            fprintf(1, "FINISH\n",i-2);
+            close(p[1]);
+            close(p[0]);
+            close(fd);
+            return;
+        }
         close(p[1]);
         close(p[0]);
         close(fd);
